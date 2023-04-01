9061417cb74ed25551c6911534f01110a5755542
Diogo Santos <diogosantosdasilva364@gmail.com>
2023-04-01 00:38:48 -0300

    Funções
    Nesta aula vimos funções que retornam funções.

---
+++ aula10.py
@@ -0,0 +1,16 @@
+"""
+Higher order Functions
+Funções de primeira classe
+"""
+
+def saudacao(msg, nome):
+    return f'{msg}, {nome}!'
+
+
+def executa(funcao, *args):
+    return funcao(*args)
+
+
+print(executa(saudacao, 'Bom dia', 'Luiz'))
+
+print(executa(saudacao, 'Boa noite', 'Maria'))
